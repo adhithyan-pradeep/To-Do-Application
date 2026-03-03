13179610bf0c5849eb8a3172f12ec89401d2c65e
Adhithyan Pradeep <adhithyanpradeep666@gmail.com>
2026-03-03 22:10:11 +0530

    Learned native basics and set up the navigation tab bar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>Adhi's react native app</Text>
+      <Text>hi</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+  content: {
+    fontSize: 52,
+  },
+});
